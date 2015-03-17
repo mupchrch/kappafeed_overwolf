a58bce6e0d2a55775eb5474469354773e022190c
Mike Upchurch <mupchrch@users.noreply.github.com>
2015-03-17 01:34:36 -0400

    Added overwolf specific css/js. Pulls main css/js from server.

---
+++ Files/kappa_ow.js
@@ -0,0 +1,23 @@
+function dragResize(edge){
+    overwolf.windows.getCurrentWindow(function(result){
+        if (result.status=="success"){
+            overwolf.windows.dragResize(result.window.id, edge);
+        }
+    });
+};
+            
+function dragMove(){
+    overwolf.windows.getCurrentWindow(function(result){
+        if (result.status=="success"){
+            overwolf.windows.dragMove(result.window.id);
+        }
+    });
+};
+            
+function closeWindow(){
+    overwolf.windows.getCurrentWindow(function(result){
+        if (result.status=="success"){
+            overwolf.windows.close(result.window.id);
+        }
+    });
+};
